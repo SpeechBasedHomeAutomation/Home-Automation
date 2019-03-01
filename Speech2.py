7aa62ac475318b69bb00ae7794bb04c404c2db42
SpeechBasedHomeAutomation <47627152+SpeechBasedHomeAutomation@users.noreply.github.com>
2019-03-01 07:53:22 +0530

    Add files via upload

---
+++ Speech2.py
@@ -0,0 +1,25 @@
+import speech_recognition as sr
+
+
+r = sr.Recognizer()
+mic= sr.Microphone()
+word=["turn on light","turn off light","Turn on fan","turn of fan"]
+
+sr.Microphone.list_microphone_names()
+print(sr.Microphone.list_microphone_names())
+
+with sr.Microphone() as source:
+    print("Speak :")
+    r.adjust_for_ambient_noise(source)
+    audio = r.listen(source)
+    print(source)
+
+    try:
+        text = r.recognize_google(audio)
+        print("You said : {}".format(text))
+    except:
+        print("Sorry could not recognize what you said")
+    if text in word:
+        print("valid command")
+    else:
+        print("invalid Command")
