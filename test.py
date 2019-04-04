ebddc880b6060d43c12692f1e3ded45664caa69c
SpeechBasedHomeAutomation <47627152+SpeechBasedHomeAutomation@users.noreply.github.com>
2019-04-04 14:52:50 +0530

    Update test.py

--- test.py
+++ test.py
@@ -8,22 +8,14 @@ import warnings
 warnings.filterwarnings("ignore")
 import time
 
-"""
-#path to training data
-source   = "development_set/"   
-modelpath = "speaker_models/"
-test_file = "development_set_test.txt"        
-file_paths = open(test_file,'r')
-"""
 # path to training data
 source = "SampleData/"
 
 # path where training speakers will be saved
 modelpath = "Speakers_models/"
 
-gmm_files = [os.path.join(modelpath, fname) for fname in
-             os.listdir(modelpath) if fname.endswith('.gmm')]
-
+gmm_files = [os.path.join(modelpath, fname) for fname in os.listdir(modelpath) if fname.endswith('.gmm')]
+             
 # Load the Gaussian gender Models
 models = [pickle.load(open(fname, 'rb')) for fname in gmm_files]
 speakers = [fname.split("/")[-1].split(".gmm")[0] for fname in gmm_files]
@@ -32,10 +24,10 @@ error = 0
 total_sample = 0.0
 
 print("Do you want to Test a Single Audio: Press '1' or The complete Test Audio Sample: Press '0' ?")
-take = int(raw_input().strip())
+take = int(input().strip())
 if take == 1:
     print("Enter the File name from Test Audio Sample Collection :")
-    path = raw_input().strip()
+    path = input().strip()
     print("Testing Audio : ", path)
     sr, audio = read(source + path)
     vector = extract_features(audio, sr)
@@ -47,8 +39,8 @@ if take == 1:
         scores = np.array(gmm.score(vector))
         log_likelihood[i] = scores.sum()
 
-    winner = np.argmax(log_likelihood)
-    print("\tdetected as - ", speakers[winner])
+    speaker = np.argmax(log_likelihood)
+    print("\tdetected as - ", speakers[speaker])
 
     time.sleep(1.0)
 elif take == 0:
@@ -71,21 +63,15 @@ elif take == 0:
             scores = np.array(gmm.score(vector))
             log_likelihood[i] = scores.sum()
 
-        winner = np.argmax(log_likelihood)
-        print
-        "\tdetected as - ", speakers[winner]
-
+        speaker = np.argmax(log_likelihood)
+        print("\tdetected as - ", speakers[speaker])
+        
         checker_name = path.split("_")[0]
-        if speakers[winner] != checker_name:
+        if speakers[speaker] != checker_name:
             error += 1
         time.sleep(1.0)
 
-    print
-    error, total_sample
+    print(error, total_sample)
     accuracy = ((total_sample - error) / total_sample) * 100
 
-    print
-    "The Accuracy Percentage for the current testing Performance with MFCC + GMM is : ", accuracy, "%"
-
-print
-"Hurrey ! Speaker identified. Mission Accomplished Successfully. "
+print( "Speaker identified ")
